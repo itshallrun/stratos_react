8384ff3e9875778343dfd5704713accb3bbd053e
„giroplus“ <21173275+giroplus@users.noreply.github.com>
2019-06-23 00:14:13 +0200

    set up router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Link} from "react-router-dom"
+
+
+
 import Dashboard from "../dashboard/dashboard"
+import FlightControl from "../flight_control/flight_control"
+import Settings from "../settings/settings"
+import Imprint from "../imprint/imprint"
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <BrowserRouter>
+
+          <div>
+              <ul>
+
+                  <li>
+                      <Link to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                      <Link to="/flug_kontrolle">Flug Kontrolle</Link>
+                  </li>
+                  <li>
+                      <Link to="/einstellungen">Einstellungen</Link>
+                  </li>
+                  <li>
+                      <Link to="/impressum">Impressum</Link>
+                  </li>
+              </ul>
+
+              <hr/>
+
+              <Route  exact path="/" component={Dashboard}/>
+              <Route  path="/dashboard" component={Dashboard}/>
+              <Route  path="/flug_kontrolle" component={FlightControl}/>
+              <Route  path="/einstellungen" component={Settings}/>
+              <Route  path="/impressum" component={Imprint}/>
+
+          </div>
+
+      </BrowserRouter>
     </div>
   );
 }
