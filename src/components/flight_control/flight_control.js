947607c1b5f6bcc3d13c9bfc50ff8202c633fea4
„giroplus“ <21173275+giroplus@users.noreply.github.com>
2019-06-22 23:52:24 +0200

    setup project structure

---
+++ src/components/flight_control/flight_control.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FlightControl extends Component {
+    render() {
+        return (
+            <div>
+                FlightControl
+            </div>
+        );
+    }
+}
+
+export default FlightControl;
---
+++ src/components/imprint/imprint.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Imprint extends Component {
+    render() {
+        return (
+            <div>
+                Imprint
+            </div>
+        );
+    }
+}
+
+export default Imprint;
