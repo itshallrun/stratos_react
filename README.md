1130d1342526e35c3c1896fa39bc1933375bb591
„giroplus“ <21173275+giroplus@users.noreply.github.com>
2019-06-23 00:44:47 +0200

    added dashboard layout & favicon, removed comments from index.html

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.<br>
 You will also see any lint errors in the console.
 
+
+
+
+## create-react-app
+## react router
+## css grid
--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
+import "./dashboard.css"
 
 class Dashboard extends Component {
     render() {
         return (
-            <div>
-                dashboard
+            <div className="grid-container">
+                <div className="map">map</div>
+                <div className="chart">chart</div>
+                <div className="live_values">
+                    <div className="val1">val</div>
+                    <div className="val2">val</div>
+                    <div className="val3">val</div>
+                    <div className="val4">val</div>
+                    <div className="val5">val</div>
+                    <div className="val6">val</div>
+                    <div className="val7">val</div>
+                    <div className="val8">val</div>
+                    <div className="val9">val</div>
+                    <div className="val10">val</div>
+                    <div className="val11">val</div>
+                    <div className="val12">val</div>
+                </div>
             </div>
         );
     }
