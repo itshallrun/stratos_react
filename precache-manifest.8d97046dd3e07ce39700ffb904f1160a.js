0c8e540ed63a81256582f1fb13971344227228dd
itshallrun <21173275+itshallrun@users.noreply.github.com>
2019-07-01 20:00:23 +0200

    Updates

---
+++ precache-manifest.8d97046dd3e07ce39700ffb904f1160a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88021ecbcac8648c6d5ccafe9d5961f6",
+    "url": "/stratos_react/index.html"
+  },
+  {
+    "revision": "587c2af7a45d0f65bc3e",
+    "url": "/stratos_react/static/css/main.ffb7e594.chunk.css"
+  },
+  {
+    "revision": "68c1bcdfe992c79bc337",
+    "url": "/stratos_react/static/js/2.67d53153.chunk.js"
+  },
+  {
+    "revision": "587c2af7a45d0f65bc3e",
+    "url": "/stratos_react/static/js/main.95c46453.chunk.js"
+  },
+  {
+    "revision": "3de09199be6199c807a2",
+    "url": "/stratos_react/static/js/runtime~main.5532e0a0.js"
+  },
+  {
+    "revision": "7443f9c9285f5d4c0e4dcf8653dbdc76",
+    "url": "/stratos_react/static/media/logo_2016.7443f9c9.png"
+  }
+]);
