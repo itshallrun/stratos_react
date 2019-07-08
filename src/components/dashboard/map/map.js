934b8b0b88b93f8f7bd492fa5c381710f3bda435
itshallrun <21173275+itshallrun@users.noreply.github.com>
2019-07-08 12:28:16 +0200

    ADD legend to map

--- src/components/dashboard/map/map.js
+++ src/components/dashboard/map/map.js
@@ -1,5 +1,6 @@
 import {GoogleMap, withGoogleMap, withScriptjs, Polyline} from "react-google-maps"
 import React from "react"
+import "./map.css"
 
 
 export default withScriptjs(withGoogleMap((props) => {
@@ -7,6 +8,20 @@ export default withScriptjs(withGoogleMap((props) => {
     return <GoogleMap
         defaultCenter={{lat: 49.2351, lng: 6.9739}}
         defaultZoom={13}>
-        <Polyline path = {props.data}/>
+        <Polyline options={{strokeColor: "green"}} path={props.data}/>
+        <div id="legend">
+            <span style={{
+                color: "green",
+                fontWeight: "bolder",
+                fontSize: " 1.2em"
+            }}>-</span> Flugroute
+            <br/>
+            <span style={{
+                color: "orange",
+                fontWeight: "bolder",
+                fontSize: " 1.2em"
+            }}>-</span> Vorhersage
+        </div>
     </GoogleMap>
 }));
+
